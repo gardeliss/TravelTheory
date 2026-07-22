d81b278aa36d8b06c9e2ee170de1f5d2a61ae1f7
gardeliss <60095468+gardeliss@users.noreply.github.com>
2026-07-22 11:39:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2296,7 +2296,7 @@ function formatDateDisplay(dateStr) {
   const dd = String(d.getDate()).padStart(2,'0');
   const mm = String(d.getMonth()+1).padStart(2,'0');
   const yyyy = d.getFullYear();
-  return `${dd}-${mm}-${yyyy}`;
+  return `${dd}${mm}${yyyy}`;
 }
 
 function toast(msg, type = 'success') {
